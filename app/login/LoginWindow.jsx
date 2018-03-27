46be4f1e5c31cb6483a9e4eb70baf2bb719e52f7
Ravi Ojha <rbojha008@gmail.com>
2018-03-27 13:23:24 +0530

    Name changing

---
+++ app/login/LoginWindow.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, FormGroup, FormControl, Checkbox, Button } from 'react-bootstrap';
+import logo from '../../assets/logo.png';
+// import PropTypes from 'prop-types';
+
+const LoginWindow = () => (
+  <div>
+    <div className="login-window" />
+    <div className="login-panel">
+      <img src={logo} alt="Logo" className="main-logo" />
+      <Form>
+        <FormGroup controlId="formHorizontalEmail">
+          <h5>Username or Email</h5>
+          <FormControl type="text" />
+        </FormGroup>
+
+        <FormGroup controlId="formHorizontalPassword">
+          <h5>Password</h5>
+          <FormControl type="password" />
+        </FormGroup>
+        <FormGroup>
+          <Link to="/forgotpassword">Forgot Password?</Link>
+        </FormGroup>
+        <FormGroup>
+          <div className="full-btn">
+            <Button type="submit" bsStyle="primary">
+              Login
+            </Button>
+          </div>
+        </FormGroup>
+        <FormGroup>
+          <Checkbox>Remember me</Checkbox>
+        </FormGroup>
+      </Form>
+    </div>
+  </div>
+);
+
+LoginWindow.propTypes = {};
+
+export default LoginWindow;
