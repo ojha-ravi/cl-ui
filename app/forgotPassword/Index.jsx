b3448ea490c2b013f9c18edeb849ac6381c9b812
Ravi Ojha <rbojha008@gmail.com>
2018-03-27 13:18:12 +0530

    Starting to work on Forgot Password

---
+++ app/forgotPassword/Index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Index = () => <div>Hello World</div>;
+
+Index.propTypes = {};
+
+export default Index;
--- app/routes.jsx
+++ app/routes.jsx
@@ -2,13 +2,15 @@ import * as React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import '../css/app.less';
-import LoginLayout from './login/index';
+import LoginLayout from './login/Index';
+import ForgotPasswordLayout from './forgotPassword/Index';
 
 // IndexRoute
 const Routes = (
   <Router>
     <div>
       <Route exact path="/" component={LoginLayout} />
+      <Route exact path="/forgotpassword" component={ForgotPasswordLayout} />
     </div>
   </Router>
 );
