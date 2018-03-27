d0efa0333244fe3a5c5d5b4b4a6a9addd0d08170
Ravi Ojha <rojha@mdsol.com>
2018-03-27 15:00:42 +0530

    Intigration with Redux is done (#7)

--- app/routes.jsx
+++ app/routes.jsx
@@ -1,18 +1,22 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import '../css/app.less';
 import LoginLayout from './login/Index';
 import ForgotPasswordLayout from './forgotPassword/Index';
+import store from './tools/configureStore';
 
 // IndexRoute
 const Routes = (
-  <Router>
-    <div>
-      <Route exact path="/" component={LoginLayout} />
-      <Route exact path="/forgotpassword" component={ForgotPasswordLayout} />
-    </div>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <div>
+        <Route exact path="/" component={LoginLayout} />
+        <Route exact path="/forgotpassword" component={ForgotPasswordLayout} />
+      </div>
+    </Router>
+  </Provider>
 );
 
 render(Routes, window.document.querySelector('.app'));
